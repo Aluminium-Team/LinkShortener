f5dc2cc19dde64063d428c73c04f418346dc620c
Eyad-Hantouli <eyadhantouli0@gmail.com>
2024-10-12 12:12:55 +0300

    remove unnecessary classes

--- src/main/java/com/aluminium/linkShortener/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.aluminium.linkShortener;
-
-import org.springframework.beans.factory.annotation.Value;
-
-public class Constants {
-
-    public static final int SERVER_ID = 1;
-}
--- src/main/java/com/aluminium/linkShortener/service/IdService.java
+++ src/main/java/com/aluminium/linkShortener/service/IdService.java
@@ -1,6 +1,5 @@
 package com.aluminium.linkShortener.service;
 
-import com.aluminium.linkShortener.Constants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
